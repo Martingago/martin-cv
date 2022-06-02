3d61add6b8efb6cf1b5b1d92fd9f89043063b8de
Martin <gagochorenmartin@gmail.com>
2022-06-02 23:35:29 +0200

    poca cosa del menu admin

--- src/views/admin/AdminIndex.vue
+++ src/views/admin/AdminIndex.vue
@@ -1,12 +1,10 @@
 <template>
-<div>
+    <span class="background-body"></span>
     <MenuAdminVue></MenuAdminVue>
-    <FormLaboralVue></FormLaboralVue>
-    <FormLaboralVue></FormLaboralVue>
-    <FormLaboralVue></FormLaboralVue>
-    <FormLaboralVue></FormLaboralVue>
-    
-</div>
+    <div class="elements-grid">
+        <FormLaboralVue></FormLaboralVue>
+        <FormLaboralVue></FormLaboralVue>
+    </div>
 
 </template>
 <script setup>
@@ -15,12 +13,29 @@ import MenuAdminVue from '@/components/admin/MenuAdmin.vue';
 </script>
 
 <style>
-
-body{
-    background: rgb(238,238,238);
-background: linear-gradient(137deg, rgba(238,238,238,1) 22%, rgba(200,200,200,1) 88%);
+.background-body {
+    width: 100%;
+    min-height: 100vw;
+    position: fixed;
+    top: 0;
+    right: 0;
+    background: rgb(238, 238, 238);
+    background: linear-gradient(137deg, rgba(238, 238, 238, 1) 22%, rgba(200, 200, 200, 1) 88%);
+    z-index: 0;
 }
-.header{
+
+.header {
     display: none;
 }
+
+.elements-grid{
+    margin-left: 200px;
+    width: calc(100% - 200px);
+    padding: 2rem;
+    display: grid;
+    justify-content: center;
+    gap: 2rem;
+    grid-template-columns: repeat(2, auto);
+}
+
 </style>
