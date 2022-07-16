c1dbcaaf56bb236833f64fc4eff2acab67487eb1
Martin <gagochorenmartin@gmail.com>
2022-07-16 02:43:30 +0200

    modo guateque

--- src/components/Navegador.vue
+++ src/components/Navegador.vue
@@ -1,11 +1,13 @@
 <template>
 
     <div class="navegador-componente">
-        <div class="header" v-bind:class="{'expanded-movile-menu ': isActive}">
+        <div class="header" v-bind:class="{ 'expanded-movile-menu ': isActive }">
             <nav class="main-menu-section">
                 <ul class="navigation-index">
                     <li>
-                        <div @click="isActive = !isActive"><font-awesome-icon :icon="['fa', 'bars']"></font-awesome-icon></div>
+                        <div>
+                            <font-awesome-icon @click="isActive = !isActive" :icon="['fa', 'bars']"></font-awesome-icon>
+                        </div>
                     </li>
                 </ul>
                 <ul class="navegation-menu">
@@ -25,17 +27,29 @@
                         <router-link @click="cerrarMenu" to="/contacto">Contacto</router-link>
                     </li>
                 </ul>
+                <ul>
+                    <li>
+                        <div @click="toggleDarkMode">Noche</div>
+                    </li>
+                </ul>
             </nav>
             <nav class="main-menu-subfooter">
                 <ul class="navegation-social">
                     <li>
-                        <a class="a-style" href="https://github.com/Martingago" target="_blank"><font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon></a>
+                        <a class="a-style" href="https://github.com/Martingago" target="_blank">
+                            <font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon>
+                        </a>
                     </li>
                     <li>
-                        <a class="a-style" href="https://www.linkedin.com/in/mart%C3%ADn-gago-chor%C3%A9n-6b1814241/" target="_blank"><font-awesome-icon :icon="['fab', 'linkedin']"></font-awesome-icon></a>
+                        <a class="a-style" href="https://www.linkedin.com/in/mart%C3%ADn-gago-chor%C3%A9n-6b1814241/"
+                            target="_blank">
+                            <font-awesome-icon :icon="['fab', 'linkedin']"></font-awesome-icon>
+                        </a>
                     </li>
                     <li>
-                        <a class="a-style" href="https://www.twitch.tv/" target="_blank"><font-awesome-icon :icon="['fab', 'instagram']"></font-awesome-icon></a>
+                        <a class="a-style" href="https://www.twitch.tv/" target="_blank">
+                            <font-awesome-icon :icon="['fab', 'instagram']"></font-awesome-icon>
+                        </a>
                     </li>
                 </ul>
             </nav>
@@ -47,12 +61,24 @@
 
 <script setup >
 import { ref } from 'vue';
-require('@/assets/css/navegador.css')
+require('@/assets/css/navegador.css');
 
-const isActive = ref(false)
+const body = document.querySelector("body");
+const darkMode = ref(false);
+const toggleDarkMode = () => {
+    darkMode.value = !darkMode.value
+    if(darkMode.value){
+        body.classList.add('dark-mode')}
+    else{
+        body.classList.remove('dark-mode');
+        console.log("a")
+    }
 
+}
+
+const isActive = ref(false);
 const cerrarMenu = () => {
-    if(document.documentElement.scrollWidth < 1080){
+    if (document.documentElement.scrollWidth < 1080) {
         isActive.value = !isActive
     }
 }
